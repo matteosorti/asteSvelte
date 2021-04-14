e519db8ff09f0dfe4958fe77bd3c4f3b3feb437a
Matteo Sorti <matteosorti@users.noreply.github.com>
2021-04-14 11:34:45 +0200

    dettaglio

--- src/Dettaglio.svelte
+++ src/Dettaglio.svelte
@@ -1,5 +1,15 @@
 <script>
     import {link} from 'svelte-spa-router'
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+
+    let opere = []
+    onMount(async()=>{
+      console.log("montato")
+      const result=await axios.get("http://localhost:8090/projectwork/rest/opera/findAllById")
+      console.log(result);
+      opere=result.data.result;
+    })
 </script>
 
 <section class="text-gray-600 body-font overflow-hidden">
@@ -7,12 +17,12 @@
       <div class="lg:w-4/5 mx-auto flex flex-wrap">
         <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
           <h3 class="text-sm title-font text-gray-500 tracking-widest">ASTALAVISTA</h3>
-          <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">titolo</h1>
-          <h2 class="text-sm title-font text-gray-500 tracking-widest">artista</h2>
+          <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">{opera.titolo}</h1>
+          <h2 class="text-sm title-font text-gray-500 tracking-widest">{opera.artista}</h2>
           <div class="flex mb-4">
             <span class="flex-grow text-yellow-500 border-b-2 border-yellow-500 py-2 text-lg px-1">Description</span>
           </div>
-          <p class="leading-relaxed mb-4">descrizione</p>
+          <p class="leading-relaxed mb-4">{opera.descrizione}</p>
           
           <div class="flex">
             <input type="text" id="valore" name="valore" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
