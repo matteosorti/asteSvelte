7ef89130b01e1390c7e3968ce8f95e207ffc4770
Matteo Sorti <matteosorti@users.noreply.github.com>
2021-04-27 15:55:10 +0200

    asdfgh

--- src/DettaglioOpera.svelte
+++ src/DettaglioOpera.svelte
@@ -5,13 +5,13 @@
     let valore="";
     
     async function offerta(){
-      const {idutente}=JSON.parse(sessionStorage.getItem("utente"));
+      const {id_utente}=JSON.parse(sessionStorage.getItem("utente"));
       const {id}=idopera;
-      console.log("utente: "+idutente);
+      console.log("utente: "+id_utente);
       console.log("idopera: "+idopera);
       console.log("id: "+id);
       const result2=await axios.post("http://localhost:8090/projectwork/rest/utenteHasOpera/update", {
-        id, idopera, idutente, valore
+        id, idopera, id_utente, valore
       })
     }
 </script>
