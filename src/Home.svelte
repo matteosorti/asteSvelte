18a15378fbb0181dd4dc93a917545bf972fbde21
matteosorti <matteosorti99.ms@gmail.com>
2021-12-03 09:34:06 +0100

    si

--- src/Home.svelte
+++ src/Home.svelte
@@ -2,7 +2,6 @@
     import {link} from 'svelte-spa-router'
 </script>
 
-<a href="/login" use:link>logout</a>
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
